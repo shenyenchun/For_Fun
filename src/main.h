154aa27e4cf59308ae067b6696888b858efb0b1e
鄭羽辰 <sleepdog@jiangshideWIFIyouzhongyong.local>
2019-11-12 20:51:14 +0800

    新增活動圖

---
+++ src/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+int addNumber(int a, int b);
+int getTriangleType(int a, int b, int c);
+
+#endif // MAIN_H_INCLUDED
---
+++ test/TestMain.cpp
@@ -0,0 +1,12 @@
+#include "../src/main.h"
+#include <assert.h>
+
+void testGetTriangleType() {
+    assert(getTriangleType(3, 4, 5) == 0);
+}
+
+int main() {
+    assert(addNumber(5, 2) == 7);
+    testGetTriangleType();
+    return 0;
+}
